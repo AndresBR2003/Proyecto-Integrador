a38bbbe885e35fd0397e4d3d1467290ca7b9f382
AndresBR2003 <bricenoandres1@hotmail.com>
2023-11-06 02:06:29 -0500

    prueba de carpetas

--- src/main/java/cibertec/edu/pe/repositorio/EstadoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package cibertec.edu.pe.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cibertec.edu.pe.modelo.EstadoUsuario;
-
-public interface EstadoRepositorio extends JpaRepository<EstadoUsuario, Integer>{
-
-}
--- src/main/java/cibertec/edu/pe/repositorio/RolRepositorio.java
+++
@@ -1,9 +0,0 @@
-package cibertec.edu.pe.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cibertec.edu.pe.modelo.Rol;
-
-public interface RolRepositorio extends JpaRepository<Rol, Long> {
-	Rol findByNombre(String nombre);
-}
