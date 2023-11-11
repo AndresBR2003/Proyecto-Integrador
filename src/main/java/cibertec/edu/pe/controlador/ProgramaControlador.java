3c814b7631141b44d90148f9263754f7f7c6447b
AndresBR2003 <bricenoandres1@hotmail.com>
2023-11-11 03:39:03 -0500

    imagen prubeav7

--- src/main/java/cibertec/edu/pe/controlador/ProgramaControlador.java
+++ src/main/java/cibertec/edu/pe/controlador/ProgramaControlador.java
@@ -70,7 +70,7 @@ public class ProgramaControlador {
                 // Guardar la imagen en una ubicación específica
                 // Puedes usar una ruta absoluta o relativa según tus necesidades
                 
-                String filePath = "apiperumejor/src/main/resources/static/imagen" + File.separator + programa.getImagenPro().getOriginalFilename();
+                String filePath = "/app/src/main/resources/static/imagen" + File.separator + programa.getImagenPro().getOriginalFilename();
                 Files.write(Paths.get(filePath), bytes);
                 
                 Programa nuevoPrograma = new Programa();
